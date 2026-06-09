10f5599beddeed8ddf2a58127afd36a09c477d8c
JeaneMirele <jeanemirele7@gmail.com>
2026-06-08 21:11:00 -0300

    feat: sending report status to Redis

--- src/main/java/com/project/async/controllers/RelatorioController.java
+++ src/main/java/com/project/async/controllers/RelatorioController.java
@@ -3,10 +3,13 @@ package com.project.async.controllers;
 import com.project.async.dto.RelatorioRequestDTO;
 import com.project.async.producer.RelatorioProducer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.time.Duration;
 import java.util.UUID;
 
 @Controller
@@ -14,12 +17,20 @@ import java.util.UUID;
 public class RelatorioController {
 
     private final RelatorioProducer producer;
+    private final StringRedisTemplate redis;
 
     @MutationMapping
     public String solicitarRelatorio(@Argument RelatorioRequestDTO request) {
        String protocolo = UUID.randomUUID().toString();
 
+       redis.opsForValue().set("relatorio:" + protocolo, "PROCESSANDO", Duration.ofHours(24));
        producer.enviarPedidoRelatorio(protocolo, request);
        return protocolo;
+    }
+
+    @QueryMapping
+    public String consultarStatus(@Argument String protocolo){
+        String status = redis.opsForValue().get("relatorio:" + protocolo);
+        return status != null ? status : "NAO_ENCONTRADO";
     }
 }
